0c60f630f6467f86e1b6f92b8fc9fe9571e3210e
JalalUddinAKbar <nisatctg56@gmail.com>
2021-05-03 16:14:53 +0600

    React very basic touch

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    let total = 0;
+    for (let i = 0; i < cart.length; i++) {
+        const product = cart[i];
+        total = total + product.price;
+        
+    }
+    let shipping = 0;
+    if (total> 100){
+        shipping = 0
+    }
+    else if (total>50){
+        shipping = 20.99
+    }
+    else if(total > 30){
+        shipping = 11.99
+    }
+    else if(total > 0){
+        shipping = 4.99
+    }
+    const formatNumber = num => {
+        const precession = num.toFixed(2);
+        return Number(precession);
+    }
+     const tax = (total/10)
+    // const productFixed = total.toFixed(2)
+     const grandTotal = total+shipping+tax;
+    
+    return (
+        <div>
+            <h2>Order Summary</h2>
+            <p>Items Ordered: {cart.length} </p>
+            <p>Product Price: {formatNumber(total)}</p>
+            <p>Shipping cost: ${shipping} </p>
+            <p>Tax: ${formatNumber(tax)}</p>
+            <p>Total Price: ${formatNumber(grandTotal)}</p>
+            
+        </div>
+    );
+};
+
+export default Cart;
