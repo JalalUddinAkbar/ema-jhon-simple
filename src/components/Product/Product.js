58ae7d739eec8b1f16f8edcdb1289a6bffa02a42
JalalUddinAkbar <55661140+JalalUddinAkbar@users.noreply.github.com>
2021-06-11 00:30:56 +0600

    Revert "second commit"

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,19 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
-import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-
-    const { img, name, seller, price, stock, key } = props.product;
+    console.log(props)
+    const { img, name, seller, price, stock } = props.product;
     return (
         <div className="single-product">
 
-            <div className="image">
+            <div>
                 <img src={img} alt="" />
             </div>
             <div>
-                <h4 className ="product-name"> <Link to = {'/product/' + key}> {name}</Link></h4>
+                <h4>{name}</h4>
                 <br />
                 <p><small>by: {seller}</small></p>
 
@@ -23,9 +22,9 @@ const Product = (props) => {
                 <p><small>Only <strong>{stock}</strong> left in stock -
                     order soon</small></p>
                 <br/>
-                {props.showAddToCart && <button className = 'addToCartBtn' 
+                <button className = 'addToCartBtn' 
                 onClick = {() => props.handleAddProduct(props.product)}> 
-                <FontAwesomeIcon icon={faShoppingCart} />   add to cart</button>}
+                <FontAwesomeIcon icon={faShoppingCart} />   add to cart</button>
             </div>
 
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,49 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import fakeData from '../../fakeData';
-import { addToDatabaseCart, getDatabaseCart } from '../../utilities/databaseManager';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
-import './Shop.css';
-import { Link } from 'react-router-dom';
+import './Shop.css'
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
-
-    useEffect(()=> {
-        const savedCart = getDatabaseCart();
-        const productKeys = Object.keys(savedCart);
-        const previousCart = productKeys.map(existKey => {
-            const product = fakeData.find(pd => pd.key === existKey);
-            product.quantity = savedCart[existKey];
-            return product;
-        })
-        setCart(previousCart);
-   
-    },[])
     const handleAddProduct = (product) => {
-        // console.log('Product added', product);
-        const toBeAdded = product.key;
-        const sameProduct = cart.find(pd => pd.key === toBeAdded)
-
-        let count = 1;
-        let newCart;
-        if(sameProduct){
-            count = sameProduct.quantity + 1;
-            sameProduct.quantity = count;
-            const others = cart.filter(pd => pd.key !== toBeAdded)
-            newCart = [...others, sameProduct]
-        }
-
-        else{
-            product.quantity = 1;
-            newCart = [...cart, product]
-        }
+        console.log('Product added', product);
+        const newCart = [...cart, product];
         setCart(newCart);
-
-       
-        addToDatabaseCart(product.key, count)
         
     }
     return (
@@ -52,8 +20,6 @@ const Shop = () => {
 
                 {
                     products.map(pd => <Product 
-                        key = {pd.key}
-                        showAddToCart = {true}
                         handleAddProduct = {handleAddProduct} 
                         product={pd}> </Product>)
                 }
@@ -61,9 +27,7 @@ const Shop = () => {
             </div>
 
             <div className="cart-container">
-                <Cart cart = {cart}>
-                <Link to = "/review"><button className ="addToCartBtn">Review Order</button></Link>
-                </Cart>
+                <Cart cart = {cart}></Cart>
             </div>
 
         </div>
